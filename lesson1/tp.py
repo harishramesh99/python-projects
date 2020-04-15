ee36224ae3d6213e4e96e8d1aedb647ce13d9fe4
harish <harish.ramesh99@gmail.com>
2020-04-15 23:12:52 +0530

    Special Case
    Signed-off-by: Shruthi01 <shruks.ramesh99@gmail.com>

---
+++ lesson1/tp.py
@@ -0,0 +1,9 @@
+a = '0987'
+b = int(a)
+c = str(b)
+
+print(b)
+print(c)
+
+x = (b,c)
+print(x)
---
+++ sp.py
@@ -0,0 +1,18 @@
+a = [6,7,8]
+b = [1,2,3,4]
+
+c = a+b 
+print(c)
+
+s = 'a b c d e f'
+z = s.split()
+print(z)
+
+if t in z:
+  print('holla amigo')
+else:   
+     print('yo soy') 
+
+
+
+
